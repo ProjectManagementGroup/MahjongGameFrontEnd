7efe2aba9b153390597ed9ddf60dd33407eeb3e1
clewang <clewang@163.com>
2017-12-26 23:52:22 +0800

    login UI and game-hall UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: [ './login.component.css' ]
+  styleUrls: [ './login.component.css']
 })
 
 export class LoginComponent {
