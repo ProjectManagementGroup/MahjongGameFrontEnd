26a83ac15411a46384aaafb289d244ef1cd3be18
mmyuan <1524495979@qq.com>
2017-12-27 15:49:59 +0800

    game.layout

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {Player} from '../object/player';
+import {PlayerService} from '../service/player.service';
+
+const PLAYERS: Player[] = [
+  {name: 'name1', gameid: 0, point: 100, ready: true},
+  {name: 'name2', gameid: 1, point: 100, ready: true},
+  {name: 'name3', gameid: 2, point: 100, ready: true},
+  {name: 'name4', gameid: 3, point: 100, ready: true}
+];
 
 @Component({
   selector: 'center-table',
@@ -7,5 +16,20 @@ import { Component } from '@angular/core';
 })
 
 export class CenterComponent {
+  private leftPlayer: Player;
+  private rightPlayer: Player;
+  private upPlayer: Player;
+  private bottomPlayer: Player;
+  private curPlayer: Player;
+  private countDown: number;
+
+  constructor(private playerSer: PlayerService ) {
+    this.leftPlayer = PLAYERS[0];
+    this.upPlayer = PLAYERS[1];
+    this.rightPlayer = PLAYERS[2];
+    this.bottomPlayer = PLAYERS[3];
+    this.curPlayer = PLAYERS[2];
+    this.countDown = 7;
+  }
 
 }
