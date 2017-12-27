933eb4a68aacd88eab010cb0e9208349251e7f1a
weina2016 <14302010056@fudan.edu.cn>
2017-12-28 00:01:52 +0800

    socket service debug

--- src/app/service/socket.service.ts
+++ src/app/service/socket.service.ts
@@ -31,25 +31,28 @@ export class SocketService {
   get_uuid: Subject<number> = new Subject<number>();
   //游戏是否开始
   game_start: boolean=false;
+  //下一个出牌者
+  next_turn:number=0;
+  get_next_turn:Subject<number> = new Subject<number>();
   //自己的index
   get_game_start: Subject<boolean> = new Subject<boolean>();
-  //发出吃碰请求的三张牌
+  //发出吃碰请求的两张牌
   eat_bump_tiles: Tile[];
-
   players: Player[]; //player列表
   get_players: Subject<Player[]> = new Subject<Player[]>();
   register_state:boolean=false;
-  getRS: Subject<boolean> = new Subject<boolean>();
+  getRS:Subject<boolean> = new Subject<boolean>();
   login_state:boolean=false;
   getLS: Subject<boolean> = new Subject<boolean>();
   //被邀请信息
-  inviation: string[];
+  inviation: string[]=[];
   get_invitation: Subject<string[]> = new Subject<string[]>();
   //成功邀请的房间号
   room_number: string;
   get_room_number: Subject<string> = new Subject<string>();
   wsUrl: string = 'wss://47.96.147.90:9000/game/ws';
-  socket: WebSocket = null;
+  //socket: WebSocket = null;
+  socket : WebSocket = new WebSocket(this.wsUrl);
   //自己将要出的牌
   out_tile: Tile;
   //自己胡的牌
@@ -63,10 +66,205 @@ export class SocketService {
   //胡的牌
   win_tiles: Tile[];
   get_win_tiles: Subject<Tile[]> = new Subject<Tile[]>();
-  private create():void {
-    this.socket = new WebSocket(this.wsUrl);
-    this.socket.onmessage = this.dealData;
+ public create():void {
+    //this.socket = new WebSocket(this.wsUrl);
+    let global = this;
+    //this.socket.onmessage = this.dealData;
+   this.socket.onmessage = function (event) {
+     //global.getRS.next(true);
+     var message = JSON.parse(event.data);
+     console.log(event.data);
+     switch (message.message) {
+       case 'register':
+         if (message.ok) {
+           console.log("register success");
+           global.register_state=true;
+           //console.log("register_state:"+this.register_state);
+           global.getRS.next(global.register_state);
+         }else{
+           global.register_state=false;
+           global.getRS.next(global.register_state);
+         }
+         break;
+       case 'login':
+         if (message.ok) {
+           global.login_state=true;
+           global.user=<User>message.object;
+           global.getLS.next(global.login_state);
+           global.get_user.next(global.user);
+         }else{
+           global.login_state=false;
+           global.getLS.next(global.login_state);
+         }
+         break;
+       case 'join random success':
+         break;
+       case 'room information':
+         var content= message.object;
+         global.players = <Player[]>content.others;
+         global.players.push(<Player>content.me);
+         global.get_players.next(global.players);
+         global.uuid=content.me.index;
+         global.get_uuid.next(global.uuid);
+         break;
+       //被邀请玩家收到消息
+       case 'invitation':
+         var content = message.object;
+         global.inviation.push(content.banker);
+         global.inviation.push(content.name0);
+         global.inviation.push(content.name1);
+         global.inviation.push(content.name2);
+         global.inviation.push(content.room);
+         global.get_invitation.next(global.inviation);
+         break;
+       case 'invite success':
+         global.room_number=message.object;
+         global.get_room_number.next(global.room_number);
+         break;
+       case "game start":
+         global.game_start=true;
+         global.get_game_start.next(global.game_start);
+       case "game start allocate":
+         var content = message.object;
+         global.uuid=content.ownIndex;
+         global.bottom_tile[0]=(<Tile[]>content.ownTiles);
+         for(var i=0;i<13;i++){
+           global.top_tile[0].push(null);
+           global.left_tile[0].push(null);
+           global.right_tile[0].push(null);
+         }
+         //TODO:其他人牌数量
+         if(global.uuid==0){
+         }else if(global.uuid==1){
+           global.left_tile[0].push(null);
 
+         }else if(global.uuid==2){
+           global.top_tile[0].push(null);
+         }else{
+           global.right_tile[0].push(null);
+         }
+         global.getTile_Left.next(global.left_tile);
+         global.getTile_Bottom.next(global.bottom_tile);
+         global.getTile_Right.next(global.right_tile);
+         global.getTile_Top.next(global.top_tile);
+         global.get_turn.next(global.turn);
+         global.get_rest.next(global.rest);
+       case 'get tile':
+         //TODO：发牌数据问题
+         global.bottom_tile[0].push(<Tile>message.object);
+         global.rest--;
+         global.get_rest.next(global.rest);
+         global.getTile_Bottom.next(global.bottom_tile);
+         global.next_turn = global.uuid;
+         global.get_next_turn.next(global.next_turn);
+         break;
+       case 'allocate tile':
+         var temp = message.object-global.uuid;
+         global.next_turn = message.object;
+         global.get_next_turn.next(global.next_turn);
+         global.rest--;
+         if(temp==1){
+           global.right_tile[0].push(null);
+           global.getTile_Right.next(global.right_tile);
+         }else if(temp==-1){
+           global.left_tile[0].push(null);
+           global.getTile_Left.next(global.left_tile);
+         }else{
+           global.top_tile[0].push(null);
+           global.getTile_Top.next(global.top_tile);
+         }
+         global.get_rest.next(global.rest);
+       case 'out success':
+         if(message.ok){
+           global.bottom_tile[0].splice(global.bottom_tile[0].indexOf( global.out_tile ,1), 1);
+           global.bottom_tile[2].push(global.out_tile);
+           global.getTile_Bottom.next(global.bottom_tile);
+           global.turn = global.uuid;
+           global.current_tile = global.out_tile;
+           global.get_turn.next(global.turn);
+           global.get_current.next(global.current_tile);
+         }
+         break;
+       case 'other out':
+         var content = message.object;
+         global.turn = content.index;
+         global.current_tile = (<Tile> content.tile);
+         global.get_turn.next(global.turn);
+         global.get_current.next(global.current_tile);
+         var temp = global.turn-global.uuid;
+         if(temp==1){
+           global.right_tile[2].push(global.current_tile);
+           global.getTile_Right.next(global.right_tile);
+         }else if(temp==-1){
+           global.left_tile[0].push(global.current_tile);
+           global.getTile_Left.next(global.left_tile);
+         }else{
+           global.top_tile[0].push(global.current_tile);
+           global.getTile_Top.next(global.top_tile);
+         }
+       case 'bump request success':
+         console.log("bump request success!!!!");
+         break;
+       case 'eat request success':
+         console.log("eat request success!!!!");
+         break;
+       case 'cut success':
+         var content = message.object;
+         //判断最后出牌人的位置,去掉它无用的牌中的一张
+         var temp = global.turn-global.uuid;
+         if(temp==1){
+           global.right_tile[2].splice(global.right_tile[2].indexOf(global.current_tile),1);
+           global.getTile_Right.next(global.right_tile);
+         }else if(temp==-1){
+           global.left_tile[2].splice(global.left_tile[2].indexOf(global.current_tile),1);
+           global.getTile_Left.next(global.right_tile);
+         }else if(temp==0){
+           global.bottom_tile[2].splice(global.bottom_tile[2].indexOf(global.current_tile),1);
+           global.getTile_Bottom.next(global.right_tile);
+         }else{
+           global.top_tile[2].splice(global.top_tile[2].indexOf(global.current_tile),1);
+           global.getTile_Top.next(global.right_tile);
+         }
+         //判断碰牌的人位置去掉吃碰人手中的牌和并增加用过的牌
+         temp = content.index-global.uuid;
+         global.next_turn = content.index;
+         global.get_next_turn.next(global.next_turn);
+         if(temp==1){
+           global.right_tile[0].splice(global.right_tile[0].indexOf(global.eat_bump_tiles[0]),1);
+           global.right_tile[0].splice(global.right_tile[0].indexOf(global.eat_bump_tiles[1]),1);
+           global.right_tile[1]=global.right_tile[1].concat(global.eat_bump_tiles);
+           global.getTile_Right.next(global.right_tile);
+         }else if(temp==-1){
+           global.left_tile[0].splice(global.left_tile[0].indexOf(global.eat_bump_tiles[0]),1);
+           global.left_tile[0].splice(global.left_tile[0].indexOf(global.eat_bump_tiles[1]),1);
+           global.left_tile[1]=global.left_tile[1].concat(global.eat_bump_tiles);
+           global.getTile_Left.next(global.left_tile);
+         }else if(temp==0){
+           global.bottom_tile[0].splice(global.bottom_tile[0].indexOf(global.eat_bump_tiles[0]),1);
+           global.bottom_tile[0].splice(global.bottom_tile[0].indexOf(global.eat_bump_tiles[1]),1);
+           global.bottom_tile[1]=global.bottom_tile[1].concat(global.eat_bump_tiles);
+           global.getTile_Bottom.next(global.bottom_tile);
+         }else{
+           global.top_tile[0].splice(global.top_tile[0].indexOf(global.eat_bump_tiles[0]),1);
+           global.top_tile[0].splice(global.top_tile[0].indexOf(global.eat_bump_tiles[1]),1);
+           global.top_tile[1]=global.top_tile[1].concat(global.eat_bump_tiles);
+           global.getTile_Top.next(global.top_tile);
+         }
+         break;;
+       case 'game end':
+         var content = message.object;
+         global.winner = <number>content.winnerIndex;
+         global.get_winner.next(global.winner);
+         global.win_tiles=<Tile[]>content.all[global.winner].ownTiles;
+         global.get_win_tiles.next(global.win_tiles);
+         for (var i=0 ; i<4;i++){
+           global.result_point.push(<number>content.all[i].point);
+         }
+         global.get_result_point.next(global.result_point);
+         break;
+     }
+   }
+    //this.getRS.next(true);
     this.socket.onerror = function () {
       console.log("warning!!!!!!!");
     };
@@ -80,8 +278,10 @@ export class SocketService {
     switch (message.message) {
       case 'register':
         if (message.ok) {
+          console.log("register success");
           this.register_state=true;
-          this.getRS.next(this.register_state);
+          //console.log("register_state:"+this.register_state);
+          //this.getRS.next(this.register_state);
         }else{
           this.register_state=false;
           this.getRS.next(this.register_state);
@@ -112,9 +312,9 @@ export class SocketService {
       case 'invatation':
         var content = message.object;
         this.inviation[0]=content.banker;
-        this.inviation[1]=content.username1;
-        this.inviation[2]=content.username2;
-        this.inviation[3]=content.username3;
+        this.inviation[1]=content.name1;
+        this.inviation[2]=content.name2;
+        this.inviation[3]=content.name3;
         this.inviation[4]=content.room;
         this.get_invitation.next(this.inviation);
         break;
