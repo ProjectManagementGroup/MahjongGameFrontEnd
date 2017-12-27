ebb7a07ab78abedff275bd4b2dc0d109e5d3cab8
wxy <14302010059@fudan.edu.cn>
2017-12-27 23:30:00 +0800

    hasCurrentTile

--- src/app/bottom/bottom.component.ts
+++ src/app/bottom/bottom.component.ts
@@ -54,6 +54,7 @@ export class BottomComponent  implements OnInit {
   private player: Player;
   // 0 using_tile 1 used_tile 2  useless_tile
   private tiles: Tile[][]=[[],[],[]];
+  // 最后出的那张牌在不在这个方位
   private hasCurrentTile: boolean = false;
   private lastUselessNum: number = 0;
 
