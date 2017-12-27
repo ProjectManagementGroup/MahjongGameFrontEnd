ff0def72ffcaeec7afdd17e1f38171d4ed9fd43b
wxy <14302010059@fudan.edu.cn>
2017-12-27 14:24:08 +0800

    Merge branch 'master' of https://github.com/ProjectManagementGroup/MahjongGameFrontEnd
    # Conflicts:
    #	src/app/game-hall/game-hall.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UpComponent } from './up/up.component';
@@ -14,6 +15,11 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { GameHallComponent } from './game-hall/game-hall.component';
 import { GameResultComponent } from './game-result/game-result.component';
+import { GameComponent } from './game/game.component';
+
+import { SocketService } from './service/socket.service';
+import { OrderTileService } from './service/order-tile.service';
+import { CheckTileService } from './service/check-tile.service';
 
 import { GameResultService } from './service/game-result.service';
 import { PlayerService } from './service/player.service';
@@ -21,9 +27,11 @@ import { UselessTileService } from './service/useless-tile.service';
 import { UsingTileService } from './service/using-tile.service';
 import { UsedTileService } from './service/used-tile.service';
 
-import { GameComponent } from './game/game.component';
-import { OrderTileService } from './service/order-tile.service';
-import { CheckTileService } from './service/check-tile.service';
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   declarations: [
@@ -42,7 +50,9 @@ import { CheckTileService } from './service/check-tile.service';
     GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     GameResultService,
@@ -51,7 +61,8 @@ import { CheckTileService } from './service/check-tile.service';
     UsedTileService,
     UsingTileService,
     OrderTileService,
-    CheckTileService
+    CheckTileService,
+    SocketService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../service/socket.service';
 
 @Component({
   selector: 'login',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
+  private username: string;
+  private password: string;
+  private login_state:boolean=null;
 
+  constructor(private socketService: SocketService) {
+  }
+
+  login(){
+    if(this.username!="" && this.password!="") {
+      let message = "login|" + this.username + "|" + this.password;
+      this.socketService.sendMessage(message);
+      this.socketService.getLS.subscribe(
+        (login_state)=>{
+          this.login_state=login_state;
+          if(this.login_state) {
+
+          }else {
+
+          }
+        }
+      );
+    }
+    else{
+      console.log("username或password不能为空");
+    }
+
+  }
 }
