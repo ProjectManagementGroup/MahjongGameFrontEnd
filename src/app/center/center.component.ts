4b4ddf6bdf938027aae338607817842b7d8cd8d3
mmyuan <1524495979@qq.com>
2017-12-28 14:32:06 +0800

    nothing

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -1,14 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {Player} from '../object/player';
 import {SocketService} from '../service/socket.service';
-/*
+
 const PLAYERS: Player[] = [
   {name: 'name1', gameid: 0, point: 100, ready: true},
   {name: 'name2', gameid: 1, point: 100, ready: true},
   {name: 'name3', gameid: 2, point: 100, ready: true},
   {name: 'name4', gameid: 3, point: 100, ready: true}
 ];
-*/
+
 @Component({
   selector: 'center-table',
   templateUrl: './center.component.html',
