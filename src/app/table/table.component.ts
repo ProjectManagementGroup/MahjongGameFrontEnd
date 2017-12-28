9e75c37aefa60beb8d0ac0cbe86b127555b0dd41
mmyuan <1524495979@qq.com>
2017-12-28 16:41:33 +0800

    去除了所有的模拟数据的注释

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,14 +3,14 @@ import {Player} from '../object/player';
 import {SocketService} from '../service/socket.service';
 import {Tile} from '../object/tile';
 import {Router} from "@angular/router";
-/*
+
 const PLAYERS: Player[] = [
   {name: 'name1', gameid: 0, point: 100, ready: true},
   {name: 'name2', gameid: 1, point: 100, ready: true},
   {name: 'name3', gameid: 2, point: 100, ready: true},
   {name: 'name4', gameid: 3, point: 100, ready: true}
 ];
-*/
+
 @Component({
   selector: 'table',
   templateUrl: './table.component.html',
@@ -45,12 +45,12 @@ export class TableComponent  implements OnInit {
 
     this.restTile = this.socketService.rest;
 
-    //this.leftPlayer = PLAYERS[0];
-    //this.upPlayer = PLAYERS[1];
-    //this.rightPlayer = PLAYERS[2];
-    //this.bottomPlayer = PLAYERS[3];
-    //this.restTile = 83;
-    //this.banker = PLAYERS[0];
+    // this.leftPlayer = PLAYERS[0];
+    // this.upPlayer = PLAYERS[1];
+    // this.rightPlayer = PLAYERS[2];
+    // this.bottomPlayer = PLAYERS[3];
+    // this.restTile = 83;
+    // this.banker = PLAYERS[0];
   }
 
   ngOnInit(): void {
