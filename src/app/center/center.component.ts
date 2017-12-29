eba5e828762d97d21b949754ea9447d2a5079a54
mmyuan <1524495979@qq.com>
2017-12-30 00:37:57 +0800

    center 定时器

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, AfterViewInit} from '@angular/core';
 import {Player} from '../object/player';
 import {SocketService} from '../service/socket.service';
 
@@ -9,21 +9,26 @@ const PLAYERS: Player[] = [
   {name: 'name4', gameid: 3, point: 100, ready: true}
 ];
 
+const COUNTDOWN: number = 5;
+
 @Component({
   selector: 'center-table',
   templateUrl: './center.component.html',
   styleUrls: [ './center.component.css' ]
 })
 
-export class CenterComponent {
+export class CenterComponent implements AfterViewInit,OnDestroy{
   @Input() leftPlayer: Player;
   @Input() rightPlayer: Player;
   @Input() upPlayer: Player;
   @Input() bottomPlayer: Player;
 
   @Input() next_turn: number;//下一个要出牌的人
-
-  private countDown: number = 7;
+  private turn: number;
+  // 定时器
+  private timer;
+  private countDown: number = 5;
+  private startTimer: boolean = false;
 
   constructor(private socketService: SocketService) {
     //this.leftPlayer = PLAYERS[0];
@@ -39,5 +44,30 @@ export class CenterComponent {
         this.next_turn = next_turn;
       }
     );
+    this.socketService.get_turn.subscribe(
+      (turn)=>{
+        this.turn = turn;
+        this.startTimer = true;
+      }
+    );
+  }
+
+  // 每一秒更新
+  ngAfterViewInit() {
+    this.timer = setInterval(() => {
+      if (this.startTimer && this.countDown > 0) {
+        this.countDown--;
+      }else{
+        this.startTimer = false;
+        this.countDown = COUNTDOWN;
+      }
+    }, 1000);
+  }
+
+  // 销毁组件时清除定时器
+  ngOnDestroy() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
   }
 }
